a9df974ddfc7f532c46d4fb185bccaa87c8da925
kartenow <67809507+kartenow@users.noreply.github.com>
2020-07-20 03:32:17 +0430

    Create alizada.sh

---
+++ alizada.sh
@@ -0,0 +1,38 @@
+!/usr/bin/bash
+clear
+printf """\e[0m\e[1;95m
+
+██████╗░██╗░░░██╗░░░░░░░█████╗░██╗░░░░░██╗███████╗░█████╗░██████╗░░█████╗░
+██╔══██╗╚██╗░██╔╝░░░░░░██╔══██╗██║░░░░░██║╚════██║██╔══██╗██╔══██╗██╔══██╗
+██████╦╝░╚████╔╝░█████╗███████║██║░░░░░██║░░███╔═╝███████║██║░░██║███████║
+██╔══██╗░░╚██╔╝░░╚════╝██╔══██║██║░░░░░██║██╔══╝░░██╔══██║██║░░██║██╔══██║
+██████╦╝░░░██║░░░░░░░░░██║░░██║███████╗██║███████╗██║░░██║██████╔╝██║░░██║
+╚═════╝░░░░╚═╝░░░░░░░░░╚═╝░░╚═╝╚══════╝╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝
+«------------AlizadA-GanG------------»
+ \e[0m\e[1;93mCreated By AlizadA\e[0m\e[1;96m( \e[0m\e[1;95mAlizadA-GanG \e[0m\e[1;96m)"""
+printf ""
+echo
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Your Name : \e[0m\e[1;96m\en' option
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
+sleep 2
+apt update
+apt install figlet -y
+cp 1 .bashrc
+echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
+echo "  figlet $option" >> .bashrc
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m"
+printf "echo -e '\e[0m\e[1;32m'◁━━━━━━━━━━━━━━━━━━━━◈✙◈━━━━━━━━━━━━━━━━━━━━▷
+echo -e '\e[0m'
+" >> .bashrc
+mv .bashrc ~
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
+sleep 2
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
+sleep 1
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Restart Termux App\e[0m"
+printf " \e[35m
+"
